68fdacfd054a93dc6ff1a3f75c9ee8e128066f8c
Waut Schepens <133114632+waut10000@users.noreply.github.com>
2024-06-05 21:46:00 +0200

    Create security.h

---
+++ ESP-32/security.h
@@ -0,0 +1,23 @@
+/*
+=============================
+    SENSITIVE INFORMATION
+=============================
+*/ 
+
+//Meter ID
+char ID[] = " {meter id} ";
+
+// WI-FI
+
+const char* networks[][2] = {
+    {"ssid1", "password1"},
+    {"ssid2", "password2"},
+    {"ssid3", "password3"},
+    {"ssid4", "password4"}
+};
+
+// MQTT CONNECTION
+const char *mqttServer = "{Node red IP}"; //Waut
+const int mqttPort = 1883;
+const char *mqttUser = "user";
+const char *mqttPassword = "password";
